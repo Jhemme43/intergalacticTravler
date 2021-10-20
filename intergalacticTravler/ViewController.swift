2bbc81a2b06b2c3b38f56c541d927a24ea3e7988
Jackson <jhhemme43@gmail.com>
2021-10-20 10:42:22 -0600

    segway completed

--- intergalacticTravler/ViewController.swift
+++ intergalacticTravler/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        assignbackground()
         // Do any additional setup after loading the view.
     }
 
+    func assignbackground(){
+           let background = UIImage(named: "starbackground")
 
+           var imageView : UIImageView!
+           imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+           imageView.clipsToBounds = true
+           imageView.image = background
+           imageView.center = view.center
+           view.addSubview(imageView)
+           self.view.sendSubviewToBack(imageView)
+    }
 }
 
